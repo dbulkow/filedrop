9ec2dbbf949f4135be2d7000a397f15f4c807508
David Bulkow <dbulkow@gmail.com>
2017-07-21 23:10:11 -0400

    add some notes about front page

--- frontpage.go
+++ frontpage.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+/*
+ upload filename text field
+ expire date/time picker - or - duration picker (1 day, week, month) no longer
+ mail address list (separate by spaces) text field
+ submit button
+*/
+
 func frontPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello world\n")
 }
