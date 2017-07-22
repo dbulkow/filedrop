967428c98575161d90d704e59baa7a1cd3a2ec52
David Bulkow <dbulkow@gmail.com>
2017-07-21 22:43:13 -0400

    add base for storage subcommands

--- storage.go
+++ storage.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -9,14 +10,63 @@ import (
 func init() {
 	storageCmd := &cobra.Command{
 		Use:   "storage",
-		Short: "Dump storage contents",
-		Long:  "Dump detailed storage contents",
-		RunE:  storage,
+		Short: "Manage file storage",
+		Long:  "Manage file storage",
 	}
+
+	listCmd := &cobra.Command{
+		Use:   "list",
+		Short: "List storage contents",
+		Long:  "List detailed storage contents",
+		RunE:  listStorage,
+	}
+
+	removeCmd := &cobra.Command{
+		Use:     "remove <filename>...",
+		Aliases: []string{"rm", "del", "delete"},
+		Short:   "Remove file(s)",
+		Long:    "Remove file(s)",
+		RunE:    removeStorage,
+	}
+
+	storageCmd.AddCommand(listCmd)
+	storageCmd.AddCommand(removeCmd)
 
 	RootCmd.AddCommand(storageCmd)
 }
 
-func storage(cmd *cobra.Command, args []string) error {
+func listStorage(cmd *cobra.Command, args []string) error {
 	return fmt.Errorf("unimplemented")
+}
+
+func removeStorage(cmd *cobra.Command, args []string) error {
+	return fmt.Errorf("unimplemented")
+}
+
+/*
+.../storage/<hash of filename, time posted>/file
+                                           /metadata
+*/
+
+type MetaData struct {
+	Filename  string
+	Hash      string
+	Created   time.Time
+	Expire    time.Time
+	Owner     string
+	MailAddrs []string
+}
+
+func (m *MetaData) Notify() {
+	// iterate over MailAddrs
+	// use smtp to provide html mail with link to server
+}
+
+func (m *MetaData) Expire() {
+	if time.Now().After(m.Expire) {
+		// remove hashdir
+	}
+}
+
+func (m *MetaData) mkhash() {
 }
