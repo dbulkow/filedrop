27725fa012a5021908eb875bbd84b12f6d145bcd
David Bulkow <dbulkow@gmail.com>
2017-07-22 12:53:53 -0400

    serve simple status

--- status.go
+++ status.go
@@ -1,6 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
 
 func status(w http.ResponseWriter, r *http.Request) {
+	log.Printf("serving status to %s", r.RemoteAddr)
+
+	now := time.Now()
+
+	for _, md := range storage.Files {
+		if md.Expire.After(now) {
+			fmt.Fprintf(w, "%s %s %s\n", md.Hash, md.Expire.Sub(now), md.Filename)
+		}
+	}
 }
