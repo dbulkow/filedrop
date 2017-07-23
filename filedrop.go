c86d6cf56cc96a5f5a9690e1bae87f28cb57759a
David Bulkow <dbulkow@gmail.com>
2017-07-22 22:33:06 -0400

    remove cobra subcommands, just use pflags, merge serve with filedrop

--- filedrop.go
+++ filedrop.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
 )
 
 var RootCmd = &cobra.Command{
@@ -13,27 +17,66 @@ var RootCmd = &cobra.Command{
 	Long:  "File sharing service",
 }
 
-const DefaultRoot = "./downloads"
+const (
+	DefaultRoot   = "./downloads"
+	DefaultPort   = "8080"
+	DefaultListen = "127.0.0.1"
+)
 
-var storage *Storage
+var (
+	storage *Storage
+	url     string
+)
 
 func main() {
 	var (
-		server = os.Getenv("FILEDROP_SERVER")
 		root   = os.Getenv("FILEDROP_ROOT")
+		listen = os.Getenv("FILEDROP_ADDRESS")
+		port   = os.Getenv("FILEDROP_PORT")
 	)
+
+	url = os.Getenv("FILEDROP_SERVER_URL")
+
+	if listen == "" {
+		listen = DefaultListen
+	}
+
+	if port == "" {
+		port = DefaultPort
+	}
+
+	if url == "" {
+		url = fmt.Sprintf("https://%s:%s", listen, port)
+	}
 
 	if root == "" {
 		root = DefaultRoot
 	}
 
-	RootCmd.PersistentFlags().StringVarP(&server, "server", "s", server, "Server address")
-	RootCmd.PersistentFlags().StringVarP(&root, "root", "r", root, "Storage directory")
+	flag.StringVarP(&root, "root", "r", root, "Storage directory")
+	flag.StringVarP(&listen, "listen", "l", listen, "Listen address")
+	flag.StringVarP(&port, "port", "p", port, "Port number")
+	flag.StringVarP(&url, "url", "u", url, "Filedrop server URL to advertise")
+
+	flag.Parse()
 
 	storage = NewStorage(root)
 
-	if err := RootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.StripPrefix("/", makeGzipHandler(frontPage)))
+	mux.HandleFunc("/status", status)
+	mux.Handle("/metrics", metrics())
+
+	srv := &http.Server{
+		Addr:           listen + ":" + port,
+		Handler:        mux,
+		ReadTimeout:    60 * time.Second,
+		WriteTimeout:   60 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+		TLSNextProto:   nil,
 	}
+
+	log.Printf("Listening on http://%s\n", srv.Addr)
+
+	log.Fatal(srv.ListenAndServe())
 }
