92ef077b5c35d3f67f91e1fe010c59c24e3ea1f7
David Bulkow <dbulkow@gmail.com>
2017-07-28 17:36:03 -0400

    give some layout to the status page

--- status.go
+++ status.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net"
 	"net/http"
-	"time"
+
+	human "github.com/dustin/go-humanize"
 )
+
+//go:generate go run scripts/mkpage.go status.html
 
 func status(w http.ResponseWriter, r *http.Request) {
 	storage.Lock()
 	defer storage.Unlock()
 
 	log.Printf("serving status to %s", r.RemoteAddr)
-
-	now := time.Now()
 
 	from, _, err := net.SplitHostPort(r.RemoteAddr)
 	if err != nil {
@@ -23,14 +25,59 @@ func status(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/plain")
+	type Files struct {
+		Filename   string
+		Size       string
+		Downloaded string
+	}
+
+	type Dir struct {
+		Hash   string
+		Expire string
+		Files  []*Files
+	}
+
+	type Status struct {
+		Dirs []*Dir
+	}
+
+	status := Status{Dirs: make([]*Dir, 0)}
 
 	for _, md := range storage.Dirs {
 		if md.From == from {
-			rem := md.Expire.Sub(now)
-			for _, f := range md.Files {
-				fmt.Fprintf(w, "%s %v %s %t\n", md.Hash, rem-(rem%time.Minute), f.Name, f.downloaded)
+			dir := &Dir{
+				Expire: human.Time(md.Expire),
+				Hash:   md.Hash,
 			}
+
+			dir.Files = make([]*Files, 0)
+			for _, f := range md.Files {
+				files := &Files{
+					Filename: f.Name,
+					Size:     human.Bytes(uint64(f.Size)),
+				}
+				if f.downloaded {
+					files.Downloaded = "downloaded"
+				}
+				dir.Files = append(dir.Files, files)
+			}
+
+			status.Dirs = append(status.Dirs, dir)
 		}
+	}
+
+	t, err := template.New("status").Parse(status_html)
+	if err != nil {
+		log.Printf("template parse: %v", err)
+		w.Header().Set("Content-Type", "text/html")
+		return
+	}
+
+	w.Header().Set("Etag", fmt.Sprintf("\"%s\"", status_etag))
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Cache-Control", "no-cache")
+	if err := t.Execute(w, status); err != nil {
+		log.Printf("template exec: %v", err)
+		return
 	}
 }
